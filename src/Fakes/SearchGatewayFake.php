0fcc1641fad32d9e4562a31b0178d5b5cae7889d
robby <robbyahn@gmail.com>
2025-06-06 17:39:48 +1200

    getResults update

--- src/Fakes/SearchGatewayFake.php
+++ src/Fakes/SearchGatewayFake.php
@@ -12,8 +12,8 @@ class SearchGatewayFake extends SearchGateway
     {
         // no-op - no need to create Guzzle client or verify that environment variables exist
     }
-
-    public function getResults(string $query, int $start, int $limit): array
+    
+    public function getResults(string $query, string $query_and, string $query_phrase, string $query_not, string $meta_t, string $meta_f_sand, string $meta_c, int $start, int $limit, string $sort): ?array
     {
         return $this->fakeResults;
     }
